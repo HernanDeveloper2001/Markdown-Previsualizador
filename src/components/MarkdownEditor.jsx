b9c2c04d3255bba3ccfca6ebdae190b96f5672fd
Hernan dario calvo <hernandariocalvo@gmail.com>
2023-10-17 12:40:32 -0500

    Terminado el redux y listo el proyecto para usar

--- src/components/MarkdownEditor.jsx
+++ src/components/MarkdownEditor.jsx
@@ -1,28 +1,68 @@
-import MarkdownPreviewer from "./MarkdownPreviewer"
-import "../styles/MarkdownEditor.scss"
-import { FaFreeCodeCamp, FaExpandArrowsAlt } from "react-icons/fa"
+import MarkdownPreviewer from "./MarkdownPreviewer";
+import "../styles/MarkdownEditor.scss";
+import { FaFreeCodeCamp, FaExpandArrowsAlt,FaCompressAlt } from "react-icons/fa";
+import { useSelector, useDispatch } from "react-redux";
+import { updateData } from "../features/data-markdown/dataMarkdown";
+import { useState } from "react";
 
 const MarkdownEditor = () => {
 
+
+  const dataMarkdown = useSelector(state => state.dataMarkdown);
+  const dataDispatch = useDispatch()
+
+  const [activeMarkdown1, setActiveMarkdown1] = useState(false);
+  const [activeMarkdown2, setActiveMarkdown2] = useState(false);
+
+  function onActiveMarkdown(id){
+   if(id === "1" ){
+    setActiveMarkdown1(!activeMarkdown1);
+    console.log(id)
+   }else if(id === "2"){
+    setActiveMarkdown2(!activeMarkdown2)
+    console.log(id)
+   }
+  }
+
+  function handleDataMarkdown({target}) {
+    const { value } = target;
+    dataDispatch(updateData(value))
+  }
+
   return (
     <>
-      <div className='editorWrap'>
-        <div className="toolbar">
-          <i className="icon-freecodecamp">
-            <FaFreeCodeCamp />
-          </i>
-            Editor
-          <i className="icon-arrows">
-            <FaExpandArrowsAlt />
-          </i>
+      <div className="editorWrap" id={activeMarkdown2 ? "hidden" : null}>
+        <div className="contenedorEditor" >
+          <div className="toolbar">
+            <i className="icon-freecodecamp">
+              <FaFreeCodeCamp />
+            </i>
+              Editor
+            <i 
+              className="icon-arrows" 
+              onClick={() => onActiveMarkdown("1")}>
+              {
+                activeMarkdown1 
+                ? <FaCompressAlt />
+                : <FaExpandArrowsAlt />
+              }
+            </i>
+          </div>
+          <textarea
+            id={activeMarkdown1?"maximized": "editor"}
+            value={dataMarkdown}
+            onChange={handleDataMarkdown}
+          />
         </div>
-        <textarea
-          id="editor"
-        />
       </div>
-      <MarkdownPreviewer />
+      <MarkdownPreviewer 
+        dataMarkdown={dataMarkdown}
+        activeMarkdown2={activeMarkdown2}
+        onActiveMarkdown={onActiveMarkdown} 
+        activeMarkdown1={activeMarkdown1} />
     </>
   )
 }
+
 
 export default MarkdownEditor
--- src/components/MarkdownPreviewer.jsx
+++ src/components/MarkdownPreviewer.jsx
@@ -1,22 +1,41 @@
-import { FaFreeCodeCamp, FaExpandArrowsAlt } from "react-icons/fa"
+import { FaFreeCodeCamp, FaExpandArrowsAlt,FaCompressAlt } from "react-icons/fa"
 import "../styles/MarkdownPreviewer.scss"
+import { marked } from "marked";
+import PropTypes from "prop-types"
 
-
-function MarkdownPreviewer() {
+function MarkdownPreviewer({ dataMarkdown,onActiveMarkdown,activeMarkdown2,activeMarkdown1 }) {
  
   return (
-    <div className="previewWrap">
-      <div className="toolbarPreview">
-        <i className="icon-freecodecamp"><FaFreeCodeCamp /></i>
-          Preview
-        <i className="icon-arrows"><FaExpandArrowsAlt /></i>
+    <div className="previewWrap" id={activeMarkdown1 ? "hidden" : null}>
+      <div className="contenedorPreview">
+        <div className="toolbarPreview">
+          <i className="icon-freecodecamp"><FaFreeCodeCamp /></i>
+            Preview
+            <i 
+              className="icon-arrows" 
+              onClick={() => onActiveMarkdown("2")}>
+              {
+                activeMarkdown2 
+                ? <FaCompressAlt />
+                : <FaExpandArrowsAlt />
+                
+              }
+            </i>
+        </div>
+        <div
+          id={activeMarkdown2?"maximized": "preview"}
+          dangerouslySetInnerHTML={{ __html: marked(dataMarkdown)}}
+        />
       </div>
-      <div
-        id="preview"
-        
-      />
     </div>
   );
+}
+
+MarkdownPreviewer.propTypes = {
+  dataMarkdown: PropTypes.string.isRequired,
+  onActiveMarkdown:PropTypes.func,
+  activeMarkdown2:PropTypes.bool,
+  activeMarkdown1:PropTypes.bool,
 }
 
 export default MarkdownPreviewer;
